6c497f5b71bd595df8786655e4c14fcaf2e9600e
Marina Kotova <m.klementyeva@mail.ru>
2024-09-09 19:05:38 +0300

    add fonts to the project, add basic layout and styles for header component

--- src/app/components/view-blocks/header/header.component.ts
+++ src/app/components/view-blocks/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchInputComponent } from '../../page-elements/search-input/search-input.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [SearchInputComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
