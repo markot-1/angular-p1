54462ff2738614c26d1a778e9ed5b0bcd6ce1e88
Marina Kotova <m.klementyeva@mail.ru>
2024-09-22 18:56:24 +0300

    create button component

--- src/app/components/page-elements/video-card/video-card.component.ts
+++ src/app/components/page-elements/video-card/video-card.component.ts
@@ -1,11 +1,12 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { ChangeBgColorPipe } from '../../../pipes/change-bg-color.pipe';
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-video-card',
   standalone: true,
-  imports: [NgIf, NgFor, ChangeBgColorPipe],
+  imports: [NgIf, NgFor, ChangeBgColorPipe, ButtonComponent],
   templateUrl: './video-card.component.html',
   styleUrl: './video-card.component.scss'
 })
--- src/app/components/view-blocks/header/header.component.ts
+++ src/app/components/view-blocks/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { SearchInputComponent } from '../../page-elements/search-input/search-input.component';
 import { LoginInformationComponent } from "../../page-elements/login-information/login-information.component";
+import { ButtonComponent } from "../../page-elements/button/button.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchInputComponent, LoginInformationComponent],
+  imports: [SearchInputComponent, LoginInformationComponent, ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
