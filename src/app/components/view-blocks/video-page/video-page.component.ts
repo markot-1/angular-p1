e448fa754bd04643f7b873071137d96f79e8535f
Marina Kotova <m.klementyeva@mail.ru>
2024-11-22 18:29:24 +0700

    add functions that receive and process data from VideoData service in video-page component

--- src/app/components/view-blocks/video-page/video-page.component.ts
+++ src/app/components/view-blocks/video-page/video-page.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { VideoDataService } from '../../../services/video-data.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-video-page',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './video-page.component.html',
-  styleUrl: './video-page.component.scss'
+  styleUrl: './video-page.component.scss',
 })
 export class VideoPageComponent {
+  data: any;
+  videoId: string | null = null;
+  videoData: any;
 
+  constructor(
+    private videoDataService: VideoDataService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.videoDataService.getData().toPromise().then((data) => {
+      this.data = data;
+      this.initializeWithData(this.data);
+    }).catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+  }
+
+  initializeWithData(data: any) {
+    this.route.paramMap.subscribe((params) => {
+      this.videoId = params.get('id');
+    });
+    let video = data.items.filter((item: any) => {
+      return item.id === this.videoId;
+    });
+    this.videoData = video[0];
+  }
 }
