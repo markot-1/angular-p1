3e15fc76a5d9cff452149dd8119591a7b493d35e
Marina Kotova <m.klementyeva@mail.ru>
2024-11-26 15:40:43 +0700

    create page-not-found component displaying a 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { MainComponent } from './components/view-blocks/main/main.component';
 import { LoginPageComponent } from './components/view-blocks/login-page/login-page.component';
 import { VideoPageComponent } from './components/view-blocks/video-page/video-page.component';
 import { appGuard } from './app.guard';
+import { PageNotFoundComponent } from './components/view-blocks/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path: 'login', title: 'Angular project | login page', component: LoginPageComponent},
@@ -12,7 +13,7 @@ export const routes: Routes = [
       component: MainComponent,
       canActivate: [appGuard],
     },
-    {path: 'video/:id', title: 'Angular project | video page', component: VideoPageComponent},
+    {path: 'video/:id', title: 'Angular project | video page', component: VideoPageComponent, canActivate: [appGuard],},
     {path: '', redirectTo: 'login', pathMatch: 'full'},
-    {path: '**', redirectTo: 'login'},
+    {path: '**', title: 'Page not found', component: PageNotFoundComponent},
 ];
