2fdcdcf267dd40e6f4ccb718fd0933503ddb850e
Marina Kotova <m.klementyeva@mail.ru>
2024-11-26 17:56:11 +0700

    add basic layout and styles for login-page component

--- src/app/components/view-blocks/login-page/login-page.component.ts
+++ src/app/components/view-blocks/login-page/login-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ButtonComponent } from '../../page-elements/button/button.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ButtonComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
